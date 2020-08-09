62c3ab0f9afc02c7eab55921160c717f95bb0c19
wuming <312467374qq.com>
2020-08-09 19:53:34 +0800

    prj initial import.

---
+++ Hello/src/com/zbondtech/Hello.java
@@ -0,0 +1,17 @@
+package com.zbondtech;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        aa();
+        System.out.println("hello");
+    }
+
+    public static void aa(){
+        System.out.println("hello");
+    }
+}
